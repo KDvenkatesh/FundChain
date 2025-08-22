0d36f565429baa4b6fa736c9625e59f8b20b188b
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-08-22 10:45:56 +0530

    Frontend Setup

--- fundchain/frontend/App.tsx
+++ fundchain/frontend/App.tsx
@@ -1,40 +1,8 @@
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
-// Internal Components
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Header } from "@/components/Header";
-import { WalletDetails } from "@/components/WalletDetails";
-import { NetworkInfo } from "@/components/NetworkInfo";
-import { AccountInfo } from "@/components/AccountInfo";
-import { TransferAPT } from "@/components/TransferAPT";
-import { MessageBoard } from "@/components/MessageBoard";
-import { TopBanner } from "@/components/TopBanner";
-
-function App() {
-  const { connected } = useWallet();
-
+export default function App() {
   return (
-    <>
-      <TopBanner />
-      <Header />
-      <div className="flex items-center justify-center flex-col">
-        {connected ? (
-          <Card>
-            <CardContent className="flex flex-col gap-10 pt-6">
-              <WalletDetails />
-              <NetworkInfo />
-              <AccountInfo />
-              <TransferAPT />
-              <MessageBoard />
-            </CardContent>
-          </Card>
-        ) : (
-          <CardHeader>
-            <CardTitle>To get started Connect a wallet</CardTitle>
-          </CardHeader>
-        )}
-      </div>
-    </>
+    <div className="App">
+      <h1>FundChain</h1>
+      {/* Add your components here */}
+    </div>
   );
 }
-
-export default App;
